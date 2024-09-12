1eeb9ace05a43682359ead40b852f0a832d6b3ad
刘程鑫 <756004757@qq.com>
2024-09-12 20:04:59 +0800

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ const StakeUnstakeTab = ({ isStaking, toggleStaking }) => {
   const cur =1
   return (
     <div className="text-white">
-      <h2 className="text-xl font-bold mb-4">{actionLabel} Your Tokens ({" account "}{cur}GC)</h2>
+      <h2 className="text-xl font-bold mb-4">{actionLabel} Your Tokens </h2>
       <div className="space-y-4">
         <input
           type="number"
@@ -221,7 +221,7 @@ export default function Home() {
         </div>
       </div>
       <div className="flex-col font-extrabold text-[50px] elative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <p>Token Address: 0xe1c78c06e415d33ded458a0c47dd17c3db1419b0</p>
+        <p>Token Address: 0x0398e1Ddc71330187c86595273EF9BC728715Ce9</p>
         <p>Your Interest: 0 GC</p>
       </div>
       <button onClick={claim} className="z-20 bg-blue-500 text-white px-6 py-3 rounded-lg cursor-pointer w-50 text-3xl">
